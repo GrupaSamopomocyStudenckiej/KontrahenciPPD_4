a5e88e86775583bb2c86f12f0a25a87bc3ead373
Szymon Wiktor <szymonwiktor+informatyk@gmail.com>
2021-02-02 16:33:58 +0100

    Gotowe (?)

--- MVC/Models/FirmyModel.cs
+++ MVC/Models/FirmyModel.cs
@@ -13,18 +13,31 @@ namespace MVC.Models
 
         [Required(ErrorMessage= "Wymagane pole")]
         public string NazwaFirmy { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string Nip { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string Regon { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string Miasto { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string Ulica { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string NrBudynku { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string NrLokalu { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string KodPocztowy { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string Poczta { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string NrTelefonu { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string Kraj { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string StronaWWW { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string NrKonta { get; set; }
     }
 }
--- MVC/Models/PracownicyModel.cs
+++ MVC/Models/PracownicyModel.cs
@@ -9,11 +9,15 @@ namespace MVC.Models
     public class PracownicyModel
     {
         public int IdPracownika { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public int IdFirmy { get; set; }
         [Required(ErrorMessage = "Wymagane pole")]
         public string Imie { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string Nazwisko { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string NrTelefonu { get; set; }
+        [Required(ErrorMessage = "Wymagane pole")]
         public string Email { get; set; }
     }
 }
